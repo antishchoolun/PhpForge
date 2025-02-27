c8e4566ea56c392f14b064744cb80d61a8213c50
Jayesh <antishchoolun95@gmail.com>
2025-02-27 10:36:34 +0400

    ewfwefew

--- src/components/icons/debug_advanced.php
+++
@@ -1,5 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2z"></path>
-    <path d="M12 18v.01"></path>
-    <path d="M12 13a2 2 0 0 0 .914-3.782 1.98 1.98 0 0 0-2.414.483"></path>
-</svg>
