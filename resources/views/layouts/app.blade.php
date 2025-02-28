587fe3ed6a62fbb3a6185a026a66bc469b35fa9d
Jayesh <antishchoolun95@gmail.com>
2025-02-28 14:28:08 +0400

    ewfew

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,27 +14,19 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans antialiased min-h-screen bg-gray-100">
+        @include('partials.header')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
+        <main class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 @isset($slot)
                     {{ $slot }}
                 @else
                     @yield('content')
                 @endisset
-            </main>
-        </div>
+            </div>
+        </main>
+
+        @include('partials.footer')
     </body>
 </html>
